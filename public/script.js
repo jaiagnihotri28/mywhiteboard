02d7b183d03b6af95974ce6aec9ffec9313d02bb
Jai Agnihotri <jai.ranimukesh12@gmail.com>
2020-03-19 15:01:41 +0530

    jj

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket=io.connect("http://localhost:3000")
+const socket=io.connect("https://mywhiteboardjai.herokuapp.com/")
 
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
